05b928b56114807710b27d642321e76d5e152f2a
Vhonn Ibo <vhonnanthonytrinidad.ibo25@bicol-u.edu.ph>
2026-09-01 22:54:51 +0800

    Update feature name

--- BillingSystem/AddCustomerForm.Designer.cs
+++ BillingSystem/AddCustomerForm.Designer.cs
@@ -187,7 +187,7 @@
             MaximizeBox = false;
             Name = "AddCustomerForm";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Billing System v1.0 - Add Customer (V.A.)";
+            Text = "Billing System v1.0 - Add Customer (V.I.)";
             ResumeLayout(false);
             PerformLayout();
         }
