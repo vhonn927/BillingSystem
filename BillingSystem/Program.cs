5d23163a3f93290f325f73eea988435360913d18
Bernard Aurellano <233678819+ABernard-2006@users.noreply.github.com>
2026-09-01 23:51:12 +0800

    Changed a comment in Program.cs on Line 5

--- BillingSystem/Program.cs
+++ BillingSystem/Program.cs
@@ -3,7 +3,7 @@ namespace BillingSystem
     internal static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        ///  The main entry point for the application. This will initialize the application and everything else.
         /// </summary>
         [STAThread]
         static void Main()
